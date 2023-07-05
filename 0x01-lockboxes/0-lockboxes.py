1eb71c2cc8f96a685e6cdd45ed60575bfadb4f3f
igraziella <igraziella2@gmail.com>
2023-07-05 12:44:10 +0000

    fixed indentation errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-""" Lockbox method """
-
+""" Method to unlock boxes """
 
 def canUnlockAll(boxes):
     """ Method that determines if all the boxes can be opened """
-
     keys = []
     opened_boxes = [False] * len(boxes)
     opened_boxes[0] = True
@@ -12,11 +10,12 @@ def canUnlockAll(boxes):
     queue = [0]
 
     while queue:
-    	current_box = queue.pop(0)
+        current_box = queue.pop(0)
         keys = boxes[current_box]
 
         for key in keys:
             if not opened_boxes[key]:
                 opened_boxes[key] = True
                 queue.append(key)
+
     return all(opened_boxes)
