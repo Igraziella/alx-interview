29720dbd248ae0e1e44bb90527d85869914724b3
igraziella <igraziella2@gmail.com>
2023-07-13 11:18:04 +0000

    modified code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,23 +2,27 @@
 """method that calculates minimum operations"""
 
 
+import math
+
 def minOperations(n):
     """calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
     if n == 1:
         return 0
-
-    operations = 0
-    factor = 2
-
-    while factor * factor <= n:
-        while n % factor == 0:
-            operations += factor
-            n /= factor
-        factor += 1
-
+    
+    # Find the divisors of n
+    divisors = []
+    for i in range(2, int(math.sqrt(n)) + 1):
+        while n % i == 0:
+            divisors.append(i)
+            n //= i
     if n > 1:
-        operations += n
-
-   return operations
+        divisors.append(n)
+    
+    if not divisors:
+        return 0
+    
+    # Calculate the minimum number of operations
+    min_ops = sum(divisors)
+    return min_ops
