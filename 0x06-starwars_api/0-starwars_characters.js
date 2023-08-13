6a52d65d26773c121d53698befd5784c6c8fde47
igraziella <igraziella2@gmail.com>
2023-08-13 14:09:25 -0700

    changed Api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,25 @@
 #!/usr/bin/node
+/**
+  prints all characters of a Star Wars movie
+  */
 
+const myArgs = process.argv.slice(2);
 const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + myArgs[0];
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: ./script.js <Movie ID>');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching movie data:', error.message);
-    return;
+request(url, async function (error, response, body) {
+  if (!error) {
+    const json = JSON.parse(body);
+    const endpoints = json.characters;
+    for (const endpoint of endpoints) {
+      await new Promise(function (resolve, reject) {
+        request(endpoint, function (error, response, body) {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
+      });
+    }
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`API request failed with status: ${response.statusCode}`);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error('Error fetching character data:', characterError.message);
-        return;
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error(`API request failed with status: ${characterResponse.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(characterBody);
-      console.log(characterData.name);
-    });
-  });
 });
